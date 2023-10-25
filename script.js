c7d0eef8cc05aecff42cc1a3f10d76be3ba24778
Aayushmaan Soni <146665161+AayushmaanSoni-45@users.noreply.github.com>
2023-10-25 20:09:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const midNav = document.querySelector(".midNav");
+    toggleMenu.classList.toggle("active");
+    midNav.classList.toggle("active");
+}
